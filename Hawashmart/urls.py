8eea950529b674e67d86c14e6d4f565400db0677
Hussein Tadicha <husseintadicha@Cexs-MacBook-Pro.local>
2025-07-11 02:28:20 +0100

    updated files

--- Hawashmart/urls.py
+++ Hawashmart/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
