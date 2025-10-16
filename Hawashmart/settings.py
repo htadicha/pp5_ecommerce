ca9b098a909edab31499280f2f9623c09b2259ba
Hussein Tadicha <husseintadicha@Cexs-MacBook-Pro.local>
2025-10-16 16:26:31 +0100

    Update settings.py, requirements.txt, and add .env.example to gitignore

--- Hawashmart/settings.py
+++ Hawashmart/settings.py
@@ -190,3 +190,25 @@ if not DEBUG:
     
     # Additional security headers
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
