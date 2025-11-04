fccddc58714728591fb7a6edb470501e45189a1f
Hussein Tadicha <husseintadicha@Cexs-MacBook-Pro.local>
2025-11-04 16:09:11 +0000

    fixed static files config

--- Hawashmart/settings.py
+++ Hawashmart/settings.py
@@ -98,6 +98,7 @@ USE_TZ = True
 
 
 # Static and Media files configuration
+# Static and Media files configuration
 if config('USE_AWS', default=False, cast=bool):
     # AWS S3 Configuration for Production
     AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
@@ -106,12 +107,27 @@ if config('USE_AWS', default=False, cast=bool):
     AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_FILE_OVERWRITE = False
-
+    AWS_DEFAULT_ACL = 'public-read'  # Important for static files to be accessible
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    
+    # Static files location
+    AWS_LOCATION = 'static'
+    
     # S3 Static and Media files configuration
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+    
+    # Static files configuration
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+    ]
+    
+    # Media files configuration
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 else:
     # Local Static and Media files configuration
